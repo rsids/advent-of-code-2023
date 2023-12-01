f8da34adf44026a4bc2126e7471dd67e37d02745
Ids Klijnsma <work@idsklijnsma.nl>
2023-12-01 10:53:06 +0100

    day 1

---
+++ day1.mjs
@@ -0,0 +1,18 @@
+import fs from "node:fs/promises";
+
+const file = await fs.readFile("data", { encoding: "utf-8" });
+const numbers = file.split("\n").reduce((prev, current, idx) => {
+  let n = current.match(/^\D*(\d).*?(\d)\D*$/);
+  console.log(n);
+  if (n === null) {
+    n = current.match(/(\d)/);
+    if (n !== null) {
+      n = [0, n[1], n[1]];
+    } else {
+      n = [0, 0, 0];
+    }
+  }
+  prev += parseInt(n[1] + n[2]);
+  return prev;
+}, 0);
+console.log(numbers);
---
+++ day2.mjs
@@ -0,0 +1,37 @@
+import fs from "node:fs/promises";
+
+const file = await fs.readFile("data", { encoding: "utf-8" });
+const nums = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+const numbers = file.split("\n").reduce((prev, current) => {
+  let n = current.match(
+    /^.*?(one|two|three|four|five|six|seven|eight|nine|\d).*(one|two|three|four|five|six|seven|eight|nine|\d).*?$/,
+  );
+  let add = 0;
+  if (n === null) {
+    n = current.match(/(one|two|three|four|five|six|seven|eight|nine|\d)/);
+    if (n !== null) {
+      add = parseInt(n[1]) || nums.indexOf(n[1]);
+      add = parseInt(`${add}${add}`);
+    }
+  } else {
+    add = parseInt(
+      `${parseInt(n[1]) || nums.indexOf(n[1])}${
+        parseInt(n[2]) || nums.indexOf(n[2])
+      }`,
+    );
+  }
+  prev += add;
+  return prev;
+}, 0);
+console.log(numbers);
