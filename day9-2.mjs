2f8abd2737097b5f44ff0c0a7347745c6a2ffa35
Ids Klijnsma <work@idsklijnsma.nl>
2023-12-09 15:47:01 +0100

    Day 9 pt2 cleanup

--- day9-2.mjs
+++ day9-2.mjs
@@ -1,25 +1,9 @@
 import fs from "node:fs/promises";
-import "core-js/actual/object/group-by.js";
 
 const file = await fs.readFile("input/day-9", {
   encoding: "utf-8",
 });
-console.time("perf");
-const result = file
-  .split("\n")
-  .filter((line) => line.trim().length > 0)
-  .map((line) => [line.split(" ").map((x) => x * 1)])
-  .map((line) => {
-    const processed = processLine(line);
-    const reduced = processed.reduce((prev, current) => {
-      current.unshift(current.at(0) - prev.at(0));
-      return current;
-    });
-    return reduced;
-  })
-  .reduce((prev, current) => prev + current.at(0), 0);
-console.log({ result });
-console.timeEnd("perf");
+
 function processLine(input) {
   const output = [];
   input[0].reduce((prev, current) => {
@@ -32,3 +16,18 @@ function processLine(input) {
   }
   return processLine(result);
 }
+
+console.time("perf");
+const result = file
+  .split("\n")
+  .filter((line) => line.trim().length > 0)
+  .map((line) => [line.split(" ").map((x) => x * 1)])
+  .map((line) =>
+    processLine(line).reduce((prev, current) => {
+      current.unshift(current.at(0) - prev.at(0));
+      return current;
+    }),
+  )
+  .reduce((prev, current) => prev + current.at(0), 0);
+console.log({ result });
+console.timeEnd("perf");
